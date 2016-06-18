da4fe7e9f5b6eb803eef6179b000e3edd97b4982
Karlla Souza <karlladanielly94@hotmail.com>
2016-06-18 12:33:42 -0300

    Update Gerente.java

--- Gerente.java
+++ Gerente.java
@@ -1,10 +1,20 @@
-package projeto.model;
+package model;
 
-public class Gerente extends Pessoa{
-	boolean isGerente=true;
-
-	public Gerente() {
-				
+public class Gerente extends Funcionario{
+	
+	public void setCodigoFuncionario(int codigoFuncionario){
+		this.codigoFuncionario=codigoFuncionario;
 	}
-
+	public int getCodigoFuncionario(){
+		return this.codigoFuncionario;
+	}
+	public void setIsGerente(boolean isGerente){
+		this.isGerente=isGerente;
+	}
+	public boolean getIsGerente(){
+		return true;
+	}
+	public void setCodigoGerente(int codigoFuncionario){
+		this.codigoFuncionario=codigoFuncionario;
+	}
 }
