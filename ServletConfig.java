2a4ef814821b9be9f8d44af2ced90859bb7c802b
Karlla Souza <karlladanielly94@hotmail.com>
2016-06-18 12:23:45 -0300

    Update ServletConfig.java

--- ServletConfig.java
+++ ServletConfig.java
@@ -1,25 +1,37 @@
-package projeto;
+package servers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+//import java.io.PrintWriter;
 //import java.io.Writer;
 
+import javax.servlet.Servlet;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ServletConfig extends HttpServlet{
-	
+@WebServlet(name = "servletConfig", urlPatterns = { "/servletConfig" })
+public class ServletConfig extends HttpServlet implements Servlet {
 	private static final long serialVersionUID = 1L;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletConfig() {
+        super();
+    }
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
 
-	@Override
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String username = getServletConfig().getInitParameter("username");
-		String email = getServletConfig().getInitParameter("email");
-		PrintWriter out = response.getWriter();
-		out.println("Nome do usuário: "+username);
-		out.println("E-mail: "+email);
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
 	}
 
 }
