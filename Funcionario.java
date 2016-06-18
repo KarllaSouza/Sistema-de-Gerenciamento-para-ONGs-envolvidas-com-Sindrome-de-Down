2ad4a00b59b571c619ac61be4db2510aba85632a
Karlla Souza <karlladanielly94@hotmail.com>
2016-06-18 12:27:29 -0300

    Update Funcionario.java

--- Funcionario.java
+++ Funcionario.java
@@ -1,10 +1,56 @@
-package projeto.model;
+package model;
+import java.util.Date;
 
-public class Funcionario extends Pessoa{
-	boolean isGerente=false;
-
-	public Funcionario() {
-		
+public class Funcionario extends Pessoa {
+	
+	public void setNome(String nome){
+		this.nome=nome;
+	}	
+	public String getNome(){
+		return this.nome;
 	}
-
+	public void setDataNasc(Date dataNasc){
+		this.dataNasc=dataNasc;
+	}
+	public Date getDataNasc(){
+		return this.dataNasc;
+	}
+	public void setCpf(String cpf){
+		this.cpf=cpf;
+	}
+	public String getCpf(){
+		return this.cpf;
+	}
+	public void setEndereco(Endereco endereco){
+		this.endereco=endereco;
+	}
+	public Endereco getEndereco(){
+		return this.endereco;
+	}
+	public void setTelefone(String telefone){
+		this.telefone=telefone;
+	}
+	public String getTelefone(){
+		return this.telefone;
+	}
+	public void setEmail(String email){
+		this.email=email;
+	}
+	public String getEmail(){
+		return this.email;
+	}
+	int codigoFuncionario;
+	boolean isGerente;
+	public void setCodigoFuncionario(int codigoFuncionario){
+		this.codigoFuncionario=codigoFuncionario;
+	}
+	public int getCodigoFuncionario(){
+		return this.codigoFuncionario;
+	}
+	public void setIsGerente(boolean isGerente){
+		this.isGerente=isGerente;
+	}
+	public boolean getIsGerente(){
+		return this.isGerente;
+	}
 }
