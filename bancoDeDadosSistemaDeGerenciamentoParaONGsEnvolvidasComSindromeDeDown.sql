b40d1ff05aecc3a6f73722cd39fd44835ecc6b6b
Karlla Souza <karlladanielly94@hotmail.com>
2016-06-23 15:03:55 -0300

    Update bancoDeDadosSistemaDeGerenciamentoParaONGsEnvolvidasComSindromeDeDown.sql

--- bancoDeDadosSistemaDeGerenciamentoParaONGsEnvolvidasComSindromeDeDown.sql
+++ bancoDeDadosSistemaDeGerenciamentoParaONGsEnvolvidasComSindromeDeDown.sql
@@ -43,7 +43,9 @@ INSERT INTO `colaborador` (`id_colaborador`, `nome_colaborador`, `cpf_colaborado
 (3, 'Diogo Figueredo', '123.456.789-01', 'diogofb.93@hotmail.com', '998010348', 0, 'Ponta Verde - Maceió / AL'),
 (5, 'Diego Henrique Melo', '986.356.345-60', 'diegomelo.90@hotmail.com', '981905902', 1, 'Maceió/AL'),
 (6, 'João Pedro Ramos Pradines', '111.222.333-44', 'pradines_jp@gmail.com', '99327-2042', 0, 'Maceió'),
-(7, 'Alexandre Henrique Cavalcanti', '222.444.555-33', 'ahc_@hotmail.com', '99676-0771', 0, 'Maceió');
+(7, 'Alexandre Henrique Cavalcanti', '222.444.555-33', 'ahc_@hotmail.com', '99676-0771', 0, 'Maceió'),
+(8, 'Raylan Medeiros', '000.999.888-77', 'raylanmedeiros@email.com', '999368273', '1', 'Campo Alegre');
+
 
 
 -- --------------------------------------------------------
@@ -61,6 +63,12 @@ CREATE TABLE `despesas` (
   `data_pagamento_despesa` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `doacao`
+--
+
+INSERT INTO despesas (cod_despesa, descricao, valor, data_vencimento_despesa, data_pagamento_despesa) VALUES ('1', 'Recursos Humanos', '51.638,41', '2016-07-23', '2016-07-23'), ('2', 'Alimentação', '293,69', '2016-06-25', '2016-06-23'), ('3', 'Manutenção/sede', '7.583,89', '2016-06-30', ''), ('4', 'Transportes', '6.915,00', '2016-07-02', ''), ('5', 'Saúde', '4.187,35', '2016-06-28', ''), ('6', 'Atividades', '4.459,67', '2016-07-07', '');
+
 
 -- --------------------------------------------------------
 
@@ -69,8 +77,10 @@ CREATE TABLE `despesas` (
 --
 
 DROP TABLE IF EXISTS `doacao`;
+
 CREATE TABLE `doacao` (
-  `id_doador` int(6) UNSIGNED NOT NULL COMMENT 'código para identificação de doador',
+  `id_doacao` int(11) NOT NULL COMMENT 'identificador da doação',
+  `id_colaborador` int(11) DEFAULT NULL COMMENT 'identificação do colaborador',
   `moeda_doacao` varchar(3) NOT NULL COMMENT 'moeda utilizada na doação. Ex.: R$, US$, etc',
   `valor_doacao` double NOT NULL COMMENT 'valor que foi doado',
   `data_doacao` date NOT NULL COMMENT 'data da doação'
@@ -80,11 +90,38 @@ CREATE TABLE `doacao` (
 -- Extraindo dados da tabela `doacao`
 --
 
-INSERT INTO `doacao` (`id_doador`, `moeda_doacao`, `valor_doacao`, `data_doacao`) VALUES
-(3, 'R$', 964, '2016-05-11'),
-(5, 'R$', 568, '2016-04-29'),
-(6, 'R$', 568, '2016-04-29'),
-(7, 'R$', 876, '2016-05-24');
+INSERT INTO `doacao` (`id_doacao`, `id_colaborador`, `moeda_doacao`, `valor_doacao`, `data_doacao`) VALUES
+(1, NULL, 'R$', 964, '2016-05-11'),
+(2, NULL, 'R$', 568, '2016-04-29'),
+(3, NULL, 'R$', 568, '2016-04-29'),
+(4, NULL, 'R$', 876, '2016-05-24'),
+(5, NULL, 'R$', 2.6, '2016-06-24'),
+(6, 4, 'R$', 647, '2016-06-06'),
+(7, 7, 'R$', 675, '2016-06-06'),
+(8, 5, 'R$', 385, '2016-06-03'),
+(9, 6, 'R$', 2.457, '2016-06-03');
+
+--
+-- Indexes for dumped tables
+--
+--
+-- Indexes for table `doacao`
+--
+ALTER TABLE `doacao`
+  ADD PRIMARY KEY (`id_doacao`);
+--
+-- AUTO_INCREMENT for dumped tables
+-- 
+AUTO_INCREMENT for table `doacao`
+--
+ALTER TABLE `doacao`
+  MODIFY `id_doacao` int(11) NOT NULL AUTO_INCREMENT COMMENT 'identificador da doação', AUTO_INCREMENT=10;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 
 
 -- --------------------------------------------------------
